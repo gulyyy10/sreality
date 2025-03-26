7464fe72bf036f047d5524e1e5acbc291486d03a
gulyyy10 <samikgulas@gmail.com>
2025-03-26 21:40:30 +0100

    Add files via upload

---
+++ components/RealityNaSplatky.tsx
@@ -0,0 +1,16 @@
+'use client';
+import React from 'react';
+
+export default function RealityNaSplatky() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Reality na splátky</h1>
+      <p>Vítej na testovací verzi webu pro zprostředkování nemovitostí!</p>
+      <ul>
+        <li>🏠 Byt 2+1 – Praha – 3.500.000 Kč</li>
+        <li>🏡 Rodinný dům – Brno – 5.800.000 Kč</li>
+        <li>🌲 Pozemek – Ostrava – 1.200.000 Kč</li>
+      </ul>
+    </div>
+  );
+}
