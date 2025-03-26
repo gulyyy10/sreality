6a14dc09a79e32bbeb160b1e39a800fc659780a8
gulyyy10 <samikgulas@gmail.com>
2025-03-26 21:52:59 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,10 @@
+// app/layout.tsx
+
+export const metadata = {
+  title: 'Reality na splátky',
+  description: 'Kompletní projekt webu pro zprostředkování nemovitostí na splátky.',
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="cs">
